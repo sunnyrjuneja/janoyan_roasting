a96db55c2d76e62aeb9970781775384c2356313c
Sunny Juneja <me@sunnyjuneja.com>
2014-05-17 11:48:50 -0700

    Add clean task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var htmlreplace = require('gulp-html-replace');
 
 var snipcart = "<script type='text/javascript' id='snipcart' data-api-key='YTQ5YWY2ZTAtNTZkZS00NDUyLTliN2ItYjRmYTJkNmM0ZTdj' src='https://app.snipcart.com/scripts/snipcart.js'></script>"
@@ -7,4 +8,9 @@ gulp.task('snipcart', function () {
   return gulp.src('site/index.html')
     .pipe(htmlreplace({'snipcart': snipcart}))
     .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src('dist/', {'read': false})
+    .pipe(clean());
 });
