6f159328a4e2f719d18df8a72539a32df8c7f804
Sunny Juneja <me@sunnyjuneja.com>
2014-05-17 11:44:03 -0700

    Create task to replace snipcart API key.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
-var useref = require('gulp-useref');
+var htmlreplace = require('gulp-html-replace');
 
-gulp.task('build', function () {
+var snipcart = "<script type='text/javascript' id='snipcart' data-api-key='YTQ5YWY2ZTAtNTZkZS00NDUyLTliN2ItYjRmYTJkNmM0ZTdj' src='https://app.snipcart.com/scripts/snipcart.js'></script>"
+
+gulp.task('snipcart', function () {
   return gulp.src('site/index.html')
-    .pipe(useref.assets())
-    .pipe(useref.restore())
-    .pipe(useref())
-    .pipe(gulp.dest('dist'));
+    .pipe(htmlreplace({'snipcart': snipcart}))
+    .pipe(gulp.dest('dist/'));
 });
