fd62e4389262de5c4fd3e89c6cef26794b2ea5ce
Sunny Juneja <me@sunnyjuneja.com>
2014-06-04 19:14:04 -0700

    Update build file to only read json files.

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var path = require('path');
 var dir = './site/coffees/';
 var output = './site/coffees/inventory.json'
 var inventory = [];
@@ -13,7 +14,7 @@ if (fs.existsSync(output)) {
 fs.readdir(dir, function(err, files){
   if (err) throw err;
   var readFiles = function(index) {
-    if(index == files.length) {
+    if (index == files.length) {
       fs.writeFile(output, JSON.stringify(inventory), function(err) {
         if(err) console.log(err);
         else console.log(JSON.stringify(inventory)), console.log("Completed.");
@@ -21,10 +22,10 @@ fs.readdir(dir, function(err, files){
     } else {
       fs.readFile(dir + files[index], 'utf-8', function(err, contents) {
         if (err) throw err;
-        else { 
+        else if (path.extname(dir + files[index]) == ".json") { 
           inventory.push(contents);
-          readFiles(index + 1);
         }
+        readFiles(index + 1);
       });
     }
   }
