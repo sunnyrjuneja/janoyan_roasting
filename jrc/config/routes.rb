da828204c0c58d993ae6a9d444b88e2fc0c4a87b
Sunny Juneja <me@sunnyjuneja.com>
2015-06-19 16:06:10 -0700

    Clean out public folder of rails app and README.

--- jrc/config/routes.rb
+++ jrc/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
   resources :coffee_products, except: :destroy
 
   # The priority is based upon order of creation: first created -> highest priority.
