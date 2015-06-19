c72e26bcedd33d1ca0b92b3be6ae3e36bfc7ab83
Sunny Juneja <me@sunnyjuneja.com>
2015-06-19 16:19:20 -0700

    Merge rails folder and angular. Change angular build to rails folder.

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,18 @@ gulp.task('make', function(callback) {
 });
 
 gulp.task('clean', function() {
-  return gulp.src('dist/', {'read': false})
+  return gulp.src('public/', {'read': false})
     .pipe(clean());
 });
 
 gulp.task('copy', function() {
   return gulp.src('site/**/**')
-    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest('public/'))
 });
 
 gulp.task('snipcart', function () {
   var snipcart = "<script type='text/javascript' id='snipcart' data-api-key='YTQ5YWY2ZTAtNTZkZS00NDUyLTliN2ItYjRmYTJkNmM0ZTdj' src='https://app.snipcart.com/scripts/snipcart.js'></script>"
   return gulp.src('site/index.html')
     .pipe(htmlReplace({'snipcart': snipcart}))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('public/'));
 });
