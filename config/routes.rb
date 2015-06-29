0bf99cc62d3de428b0c1b9b346722ac1b3dec509
Sunny Juneja <me@sunnyjuneja.com>
2015-06-29 13:25:17 -0700

    Remove nodejs dep.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
   match '/404', to: 'errors#file_not_found', via: :all
   match '/422', to: 'errors#unprocessable', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
